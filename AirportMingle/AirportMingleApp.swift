105c6e34faec448e83cdc5f18f748a581ab6cdc3
Jippe Joosten <jippe.joosten@me.com>
2021-08-18 15:48:50 +0200

    Initial xcode project

---
+++ AirportMingle/AirportMingleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirportMingleApp.swift
+//  AirportMingle
+//
+//  Created by Jippe Joosten on 18/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct AirportMingleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
